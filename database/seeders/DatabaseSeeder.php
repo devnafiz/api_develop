1d2c0fa51400155c436a6e12183f325c6700db5c
devnafiz <nafiz.2011@yahoo.com>
2021-09-13 17:19:12 +0600

    factory data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Product::factory(20)->create();
+         \App\Models\Review::factory(50)->create();
+
+        //factory(App\Models\Product::class,50)->create();
+        //factory(App\Models\Review::class,150)->create();
     }
 }
